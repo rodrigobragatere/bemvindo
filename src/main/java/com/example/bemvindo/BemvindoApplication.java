eefb577fd3c9244d0f731dc109be0c68cc6d1e70
rodrigobragatere <rodrigobragatere@gmail.com>
2024-10-20 19:20:28 -0300

    primeiro commit

---
+++ src/main/java/com/example/bemvindo/BemvindoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bemvindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemvindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemvindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bemvindo/BemvindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bemvindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemvindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
